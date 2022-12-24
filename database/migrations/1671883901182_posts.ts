ade3a5ead7672fe92de896917a7749339692b9a1
Andi Fasaya Yaqhsya Ma'ruf <53859179+fasaya@users.noreply.github.com>
2022-12-24 20:52:12 +0800

    first commit

---
+++ database/migrations/1671883901182_posts.ts
@@ -0,0 +1,30 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'posts'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.integer('user_id').unsigned().references('id').inTable('users')
+      table.integer('parent_id').nullable()
+      table.string('title').nullable()
+      table.string('slug').notNullable()
+      table.text('body').nullable()
+      table.timestamp('published_at', { useTz: true })
+
+      table.string('meta_title').nullable()
+      table.text('meta_description').nullable()
+
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
